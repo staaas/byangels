8f41cdaba6096708919b45fe9d828ca432f5f91b
Stas Rudakou <dev@nott.cc>
2011-07-17 21:13:17 +0300

    Initial commit

---
+++ src/angels/admin.py
@@ -0,0 +1,77 @@
+# -*- coding:utf-8 -*-
+from django.contrib import admin
+from django.contrib.admin.views.main import ChangeList
+from django.utils.safestring import mark_safe
+
+from models import Helper, Message, Human, Place
+
+# class ChangeAdmin(admin.ModelAdmin):
+#     pass
+# admin.site.register(Change, ChangeAdmin)
+
+class HelperAdmin(admin.ModelAdmin):
+    list_display = ['who', 'whom', 'how', 'ctime', 'visible']
+    list_filter = ['visible']
+    fields = ['who', 'whom', 'how', 'ctime', 'visible']
+    readonly_fields = ['ctime']
+admin.site.register(Helper, HelperAdmin)
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'desc', 'ip', 'ctime']
+    fields = ['title', 'desc', 'ip', 'ctime']
+    readonly_fields = ['ctime']
+    ordering = ['-ctime']
+admin.site.register(Message, MessageAdmin)
+
+class HumanChangeList(ChangeList):
+    def get_results(self, *args, **kwargs):
+        ret = super(HumanChangeList, self).get_results(*args, **kwargs)
+
+        helpers = Helper.objects.filter(
+            whom__id__in=[human.id for human in self.result_list])
+
+        human_helpers = {}
+        for helper in helpers:
+            this_human_helpers = human_helpers.get(helper.whom.id, [])
+            this_human_helpers.append(helper)
+            human_helpers[helper.whom.id] = this_human_helpers
+
+        for human in self.result_list:
+            human.cool_helpers = human_helpers.get(human.id, [])
+
+        return ret
+
+class HumanAdmin(admin.ModelAdmin):
+    list_display = ['title', 'place', 'days', 'detention', 'desc',
+                    'helpers_display', 'mtime']
+    list_display_links = ['title']
+    list_filter = ['place', 'days']
+    fields = ['title', 'place', 'days', 'detention', 'desc', 'new', 'visible',
+              'ctime', 'mtime']
+    readonly_fields = ['id', 'ctime', 'mtime', 'new']
+    actions = []
+
+    def get_changelist(self, *args, **kwargs):
+        return HumanChangeList
+
+    def helpers_display(self, obj):
+        helpers_list = getattr(obj, 'cool_helpers', [])
+        if not helpers_list:
+            return ''
+        helpers_format = (u'<i>%s:</i><br />%s' % (mark_safe(helper.who),
+                                                   mark_safe(helper.how))\
+            for helper in helpers_list)
+
+        return '<ol><li>%s</li></ol>' % '</li><li>'.join(helpers_format)
+    helpers_display.verbose_name = 'Helpers'
+    helpers_display.allow_tags = True
+
+admin.site.register(Human, HumanAdmin)
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    fields = ['title']
+admin.site.register(Place, PlaceAdmin)
+
+
+
---
+++ src/angels/models.py
@@ -0,0 +1,85 @@
+# -*- coding:utf-8 -*-
+from datetime import datetime
+
+from django.db import models
+
+BOOLEAN_CHOICES = [(0, 'да'), (1, 'нет')]
+
+class Change(models.Model):
+    id = models.IntegerField(primary_key=True)
+    field = models.CharField(max_length=765, blank=True)
+    content = models.TextField(blank=True)
+    new_content = models.TextField()
+    mtime = models.DateTimeField(null=True, blank=True)
+    ip = models.CharField(max_length=48, blank=True)
+    guy = models.IntegerField(null=True, blank=True)
+    class Meta:
+        db_table = u'changes'
+
+    def __unicode__(self):
+        return 'Change on %s' % self.field
+
+class Helper(models.Model):
+    id = models.IntegerField(primary_key=True)
+    who = models.CharField(max_length=765, verbose_name='Кто')
+    whom = models.ForeignKey('Human', db_column='whom', verbose_name='Кому')
+    how = models.TextField(verbose_name='Помощь')
+    ctime = models.DateTimeField(verbose_name='Время отправки',
+                                 auto_now_add=True)
+    visible = models.BooleanField(verbose_name='Скрыть',
+                                  choices=BOOLEAN_CHOICES,
+                                  default=0)
+    class Meta:
+        db_table = u'helpers'
+        verbose_name = 'Ангел'
+        verbose_name_plural = 'Ангелы'
+
+    def __unicode__(self):
+        return u'%s (%s)' % (self.who, self.whom)
+
+class Message(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.TextField('Заголовок')
+    desc = models.TextField('Текст')
+    ctime = models.DateTimeField(verbose_name='Время отправления', auto_now_add=True)
+    ip = models.CharField(max_length=48, verbose_name='IP адрес')
+    class Meta:
+        db_table = u'messages'
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
+
+    def __unicode__(self):
+        return self.title
+
+class Human(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=765, verbose_name='Фамилия, имя')
+    days = models.CharField(max_length=765, verbose_name='Сколько суток', blank=True)
+    place = models.ForeignKey('Place', db_column='place', blank=True, null=True, verbose_name='Где сидит (сидел)')
+    desc = models.TextField(verbose_name='Описание')
+    ctime = models.DateTimeField(verbose_name='Время создания',
+                                 auto_now_add=True)
+    mtime = models.DateTimeField(verbose_name='Последнее изменение',
+                                 auto_now=True)
+    visible = models.BooleanField(verbose_name='Скрыть',
+                                  choices=BOOLEAN_CHOICES, default=0)
+    new = models.BooleanField(choices=BOOLEAN_CHOICES, default=0)
+    detention = models.DateField(null=True, blank=True, verbose_name='Начало срока')
+    class Meta:
+        db_table = u'people'
+        verbose_name = 'Человек'
+        verbose_name_plural = 'Список людей'
+
+    def __unicode__(self):
+        return self.title
+
+class Place(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=765)
+    class Meta:
+        db_table = u'places'
+        verbose_name = 'Место отсидки'
+        verbose_name_plural = 'Места отсидки'
+
+    def __unicode__(self):
+        return self.title
